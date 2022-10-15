5fc053266698b89fc41c233c77c817ac4742bda4
Monique McIntyre <skaiwilliams85@gmail.com>
2022-10-15 03:36:34 -0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { GlobalStyles } from "./styles/Global.css";
+
+const App = () => {
+  return (
+    <div>
+      <GlobalStyles />
+      App
+    </div>
+  );
+};
+
+export default App;
